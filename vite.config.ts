e27994b41c3a51516329540c25fafd823ac2db32
zeehaancode21 <zeehaancode@gmail.com>
2026-06-01 17:01:59 +0530

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/IKT-website-finallised/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
